ae9b7cfe5a15a95af14b1c4950295174309d187d
Charlie-McDaddy <42277051+Charlie-McDaddy@users.noreply.github.com>
2023-09-21 09:58:21 +1000

    complete question set data file for survey

--- src/data/onlineCognition/questionSetOne.jsx
+++ src/data/onlineCognition/questionSetOne.jsx
@@ -1,4 +1,109 @@
-const questionSetOne = () => {
-  return <div>questionSetOne</div>;
-};
-export default questionSetOne;
+const questions = [
+  { id: 1, description: "I am most comfortable online" },
+  { id: 2, description: "Few people love me other than those I know online" },
+  { id: 3, description: "I feel safest when I am on the internet" },
+  {
+    id: 4,
+    description:
+      "I often keep thinking about something I experienced online well after I have logged off",
+  },
+  {
+    id: 5,
+    description:
+      'When I am on the Internet, I often feel a kind of "rush" or emotional high',
+  },
+  {
+    id: 6,
+    description:
+      "You can get to know a person better on the Internet than in person",
+  },
+  { id: 7, description: "I often find it peaceful to be online" },
+  { id: 8, description: "I can be myself online" },
+  { id: 9, description: 'I get more respect online than "in real life"' },
+  { id: 10, description: "I use the Internet more than I ought to" },
+  { id: 11, description: "People complain that I use the Internet too much" },
+  { id: 12, description: "I never stay on longer than I had planned" },
+  { id: 13, description: "People accept me for who I am online" },
+  {
+    id: 14,
+    description:
+      "Online relationships can be more fulfilling than offline ones",
+  },
+  {
+    id: 15,
+    description: "When I am not online, I often think about the Internet",
+  },
+  { id: 16, description: "I am at my best when I am online" },
+  {
+    id: 17,
+    description:
+      "The offline world is less exciting than what you can do online",
+  },
+  {
+    id: 18,
+    description:
+      "I wish my friends and family knew how people regard me online",
+  },
+  { id: 19, description: 'The Internet is more "real" than real life' },
+  {
+    id: 20,
+    description: "When I am online I do not think about my responsibilities",
+  },
+  { id: 21, description: "I cannot stop thinking about the Internet" },
+  { id: 22, description: "I am less lonely when I am online" },
+  {
+    id: 23,
+    description: "I cannot see myself ever without the Internet for too long",
+  },
+  { id: 24, description: "The Internet is an important part of my life" },
+  {
+    id: 25,
+    description: "I feel helpless when I do not have access to the Internet",
+  },
+  {
+    id: 26,
+    description:
+      "I say or do things on the Internet that I could never do offline",
+  },
+  { id: 27, description: "When I have nothing better to do, I go online" },
+  {
+    id: 28,
+    description:
+      "I find that I go online more when I have something else I am supposed to do",
+  },
+  {
+    id: 29,
+    description:
+      "When I am online, I do not need to think about offline problems",
+  },
+  { id: 30, description: "I sometimes use the Internet to procrastinate" },
+  {
+    id: 31,
+    description: "When I am online, I can be carefree",
+  },
+  {
+    id: 32,
+    description: "I often use the Internet to avoid doing unpleasant things",
+  },
+  {
+    id: 33,
+    description:
+      "Using the Internet is a way to forget about the things I must do but really do not want to do",
+  },
+  {
+    id: 34,
+    description:
+      "Even though there are times when I would like to, I cannot cut down on my use of the Internet",
+  },
+  {
+    id: 35,
+    description:
+      "I am bothered by my inability to stop using the Internet so much",
+  },
+  {
+    id: 36,
+    description: "My use of the Internet sometimes seems beyond my control",
+  },
+];
+
+export default questions;
