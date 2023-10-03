c7b0a92e78ab137d4dda559b57aa1a0e11f6db63
Charlie-McDaddy <42277051+Charlie-McDaddy@users.noreply.github.com>
2023-10-03 13:11:23 +1000

    module creation

---
+++ src/components/Module.jsx
@@ -0,0 +1,37 @@
+const Module = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row align-items-start">
+          <div className="col">
+            <div className="btn-container"></div>
+          </div>
+          <div className="col">
+            <article>
+              <h2 className="secondary-color">What is self-care?</h2>
+              <p>
+                Self-care means being mindful of our own needs to improve our
+                own physical, mental and emotional health. Self-care can take
+                many different forms, ranging from making sure we get enough
+                sleep to getting some fresh air for a few minutes. We’ll explain
+                some of the different types of self-care and how they can help
+                you.
+              </p>
+              <br />
+              <h2 className="secondary-color">Why is self-care important?</h2>
+              <p>
+                Self-care is particularly important when working through the
+                information on this website. This work can be difficult,
+                especially when feeling other strong emotions like anxiety or
+                fear about the future. Making sure we find time for self-care
+                can help us deal with the challenges we face in a positive way.
+              </p>
+            </article>
+            <Button />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Module;
---
+++ src/pages/selfHelp/self-care.jsx
@@ -0,0 +1,28 @@
+import { FaFilePdf } from "react-icons/fa";
+import { FcPrint } from "react-icons/fc";
+import Heading from "../components/Heading";
+import MainContent from "../components/MainContent";
+
+const SelfCareModule = () => {
+  return (
+    <>
+      <Heading>
+        <h2 className="secondary-color">Self care</h2>
+        <FcPrint />
+        <FaFilePdf />
+      </Heading>
+      <MainContent>
+        <h4 className="secondary-color">Objectives</h4>
+        <p>This module will help you explore and understand:</p>
+        <ul className="content-list">
+          <li>what self-care is and why it's important</li>
+          <li>basic self-care strategies</li>
+          <li>how to create a self-care plan.</li>
+        </ul>
+        <Module />
+        <SelfCare />
+      </MainContent>
+    </>
+  );
+};
+export default SelfCareModule;
