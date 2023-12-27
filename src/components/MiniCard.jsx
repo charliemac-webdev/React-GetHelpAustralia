ef521e0042339257d039c94b34a09cfba6c056a5
Charlie-McDaddy <42277051+Charlie-McDaddy@users.noreply.github.com>
2023-12-27 18:05:56 +1000

    update MiniCard component

--- src/components/MiniCard.jsx
+++ src/components/MiniCard.jsx
@@ -1,7 +1,7 @@
 const MiniCard = (props) => {
   return (
     <>
-      <div class="folder">
+      <div className="folder">
         <h3 className="secondary-color">{props.title}</h3>
       </div>
     </>
