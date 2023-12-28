3d0f5612fc7896711ed99b980d7623c346cf37ce
belindajp <142566486+belindajp@users.noreply.github.com>
2023-12-28 14:18:50 +1000

    Sel talk module update

--- src/data/modules/Wellbeing/selfTalkModuleData.jsx
+++ src/data/modules/Wellbeing/selfTalkModuleData.jsx
@@ -14,6 +14,8 @@ const selfTalkModuleData = [
     title: "Self-talk",
     description: (
       <>
+        <br></br>
+        <h4 className="secondary-color">Self-talk</h4>
         <p>
           What you say to yourself in your mind is called self-talk. It can have
           a really great impact on your self-esteem and confidence. How you talk
@@ -26,86 +28,53 @@ const selfTalkModuleData = [
           <li>your behaviour</li>
           <li>your view of the world</li>
         </ul>
-        <br />
         <p>We can talk to ourselves in negative and positive ways.</p>
         <hr />
         <h5 className="secondary-color">Starting Point</h5>
         <p>
-          Identify your current level of knowledge and understanding around your
-          use of self-talk (1 = very little understanding; 2 = some
-          understanding; 3 = secure understanding).
+          Take a minute and think about what you’ve said to yourself today. Was
+          it critical? Or was it kind and helpful? How did you feel after you
+          engaged in this inner discussion?
         </p>
-        <table className="table table-bordered">
-          <tbody>
-            <tr>
-              <td>
-                I have a clear understanding of the self-talk messages I give
-                myself.
-              </td>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>
-                I can see how my self-talk gives me permission to engage in
-                illegal online sexual behaviour.
-              </td>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>I understand how to start changing my self-talk.</td>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-          </tbody>
-        </table>
-        <br />
         <p>
-          If you have scored yourself as 1 or 2, then this module will be
-          particularly helpful for you.
+          Your thoughts are the source of your emotions and mood. The
+          conversations you have with yourself can be destructive or beneficial.
+          They influence how you feel about yourself and how you respond to
+          events in your life.
         </p>
+
         <hr />
         <h5 className="secondary-color">Negative self-talk</h5>
         <p>
-          There are two kinds of negative self-talk; the kind that justifies
-          inappropriate or unhealthy behaviour and the type that makes you feel
-          useless or small.
+          We all have an inner critic. At times this little voice can actually
+          be helpful and keep us motivated toward goals—like when it reminds us
+          that what we’re about to eat isn’t healthy or what we’re about to do
+          may not be wise. However, this voice can often be more harmful than
+          helpful.
         </p>
-        <br />
         <p>
-          Examples of self-talk that justify inappropriate or unhealthy
-          behaviour:
+          This statement show negative thoughts can grow and become
+          self-defeating:
         </p>
-        <img
-          src={pictureOne}
-          alt="self-talk 1"
-          style={{ height: "auto", maxWidth: "100%" }}
-        />
-        <br />
-        <br />
         <p>
-          Examples of self-talk statements that make you feel useless or small
-          include:
+          “I didn’t say the right thing at work today. I never say the right
+          thing. Everyone hates me. No wonder I can’t make friends. I’ll never
+          have friends.”
         </p>
+        <p>
+          Focusing on negative thoughts may lead to decreased motivation as well
+          as greater feelings of helplessness and sometimes depression. For some
+          people it also contributes to their sexual offending as they use it to
+          justify negative behaviours, so it’s definitely something to fix.
+        </p>
+        <br />
         <img
           src={pictureTwo}
           alt="self-talk 2"
-          style={{ height: "auto", maxWidth: "100%" }}
+          style={{ height: "auto", maxWidth: "70%" }}
         />
         <br />
-        <br />
-        <p>Negative self-talk = </p>
-        <ul className="content-list">
-          <li>negative attitude</li>
-          <li>negative feelings</li>
-          <li>negative self-image</li>
-          <li>negative behaviour</li>
-          <li>a negative view of the world</li>
-        </ul>
+        <br></br>
         <hr />
         <h5 className="secondary-color">Positive self-talk</h5>
         <p>
@@ -117,15 +86,16 @@ const selfTalkModuleData = [
         <img
           src={pictureThree}
           alt="Self-talk 3"
-          style={{ height: "auto", maxWidth: "100%" }}
+          style={{ height: "auto", maxWidth: "70%" }}
         />
         <br />
         <br />
+        <br></br>
         <p>Examples of self-talk that enhances your self-esteem:</p>
         <img
           src={pictureFour}
           alt="Self-talk 4"
-          style={{ height: "auto", maxWidth: "100%" }}
+          style={{ height: "auto", maxWidth: "70%" }}
         />
         <br />
         <br />
@@ -139,8 +109,10 @@ const selfTalkModuleData = [
         </ul>
         <br />
         <p>
-          Positive self-talk will help you to make positive changes in your
-          life.
+          <b>
+            Positive self-talk will help you to make positive changes in your
+            life.
+          </b>
         </p>
       </>
     ),
@@ -150,6 +122,7 @@ const selfTalkModuleData = [
     title: "Changing negative to positive self-talk",
     description: (
       <>
+        <br></br>
         <h4 className="secondary-color">
           Changing negative to positive self-talk
         </h4>
@@ -160,36 +133,17 @@ const selfTalkModuleData = [
           struggle to recognise them and find it hard to give yourself credit
           for them.
         </p>
-        <br />
-        <p>
-          Keep a diary for a week of any negative things you say to yourself.
-        </p>
-        <br />
-        <p>
-          After a week look back and see what sort of messages you give
-          yourself.
-        </p>
-        <br />
-        <p>
-          Would you say these things to a friend? We are often harder on
-          ourselves that others. It’s time to be your new best friend and be
-          kind to yourself.
-        </p>
-        <br />
-        <p>You need to start changing negative statements.</p>
-        <br />
         <p>
           You should now be aware that negative thinking can lead to low mood,
           loss of motivation and that it contributes to negative unhealthy
           behaviour – including in some cases offending.
         </p>
-        <br />
         <p>
           Here are some methods which can help you to tackle your negative
           thinking:
         </p>
         <p>
-          <span className="secondary-color">1</span>{" "}
+          <span className="secondary-color">1. </span>{" "}
           <b>Challenge your thinking.</b> For each negative statement ask
           yourself these questions:
         </p>
@@ -199,9 +153,8 @@ const selfTalkModuleData = [
           <li>How likely is this to be the case?</li>
           <li>If it concerned someone else what would I think?</li>
         </ul>
-        <br />
         <p>
-          <span className="secondary-color">2</span>{" "}
+          <span className="secondary-color">2. </span>{" "}
           <b>
             Do something that will distract you from negative thoughts and
             feelings.
@@ -209,20 +162,20 @@ const selfTalkModuleData = [
           This might be an activity or contacting a friend.
         </p>
         <p>
-          <span className="secondary-color">3</span>
+          <span className="secondary-color">3. </span>
           <b>Positive reframing.</b> Try to find a positive aspect to the
           situation to focus on, rather than the negative. This is something we
           often do after a bereavement for example, remembering the positive
           life someone had rather than the loss.
         </p>
         <p>
-          <span className="secondary-color">4</span>
+          <span className="secondary-color">4. </span>
           <b>Use positive language.</b> If you constantly say “I can’t” you will
           convince yourself that it’s true. Replace negative words with positive
           ones.
         </p>
         <p>
-          <span className="secondary-color">5</span>
+          <span className="secondary-color">5. </span>
           <b>
             Reflect on what has contributed to the negative thoughts and
             feelings.
@@ -233,10 +186,15 @@ const selfTalkModuleData = [
           positively.
         </p>
         <p>
-          <span className="secondary-color">6</span>
+          <span className="secondary-color">6. </span>
           <b>Don’t be too hard on yourself.</b> It takes time, practice and
           determination to change negative thinking and adopt a more optimistic
           approach.
+        </p>
+        <p>
+          <span className="secondary-color">7. </span>
+          <b>Catch it, Check it, change it </b>– the more you start spotting and
+          changing your negative talk to positive the better you will get!
         </p>
         <hr />
         <h5 className="secondary-color">Planning to be positive</h5>
@@ -249,8 +207,10 @@ const selfTalkModuleData = [
         <img
           src={pictureFive}
           alt="self-talk"
-          style={{ height: "auto", maxWidth: "100%" }}
+          style={{ height: "auto", maxWidth: "70%" }}
         />
+        <br></br>
+        <br></br>
         <p>
           This planning can be used in any situation. Think of something you are
           doing this week that you are worried or nervous about and think of
@@ -258,8 +218,9 @@ const selfTalkModuleData = [
           prepare yourself positively.
         </p>
         <hr />
+        <br></br>
         <h5 className="secondary-color">
-          How does this apply to illegal online sexual behaviour?
+          How does this apply to looking at sexual images of children?
         </h5>
         <p>
           This model is used to explain the internal argument or self-talk
@@ -271,9 +232,10 @@ const selfTalkModuleData = [
         <img
           src={pictureSix}
           alt="self-talk yes or no"
-          style={{ height: "auto", maxWidth: "100%" }}
+          style={{ height: "auto", maxWidth: "70%" }}
         />
         <br />
+        <br></br>
         <p>
           <b>Please consider the following questions:</b>
         </p>
@@ -290,58 +252,21 @@ const selfTalkModuleData = [
     title: "Reflection",
     description: (
       <>
+        <br></br>
+        <h4 className="secondary-color">Relfection</h4>
         <p>
-          As with the Introduction to this module, identify your level of
-          knowledge and understanding around the following aspects of how you
-          use illegal images (1 = very little understanding; 2 = some
-          understanding; 3 = secure understanding).
+          Keep a diary for a week of any negative things you say to yourself.
         </p>
-        <table className="table table-bordered">
-          <tr>
-            <td>
-              I have a clear understanding of the self-talk messages I give
-              myself.
-            </td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <td>
-              I can see how my self-talk gives me permission to engage in
-              illegal online sexual behaviour.
-            </td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <td>I understand how to start changing my self-talk.</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-          </tr>
-        </table>
-        <br />
-        <p>Now, consider the following questions:</p>
-        <ul className="content-list">
-          <li>
-            Have any of your responses changed from 1 to 2 or 2 to 3 from the
-            start of this module?
-          </li>
-          <li>
-            Which part of the module has had the greatest impact on your
-            understanding? Why?
-          </li>
-          <li>
-            Has anything from this module prompted or encouraged you to take
-            action around your thinking? If so, what? Has this module raised any
-            further questions for you or made you want to explore any ideas
-            further? What steps do you plan to take to seek out this
-            information?
-          </li>
-        </ul>
-        <br />
+        <p>
+          After a week look back and see what sort of messages you give
+          yourself.
+        </p>
+        <p>
+          Would you say these things to a friend? We are often harder on
+          ourselves that others. It’s time to be your new best friend and be
+          kind to yourself.
+        </p>
+        <p>You need to start changing negative statements.</p>
         <p>
           If you want to discuss anything covered in this module, have struggled
           with working through the self-help material or just want the
