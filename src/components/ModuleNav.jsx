37f25950dc31a8347928e765f999897ce4ad7313
Charlie-McDaddy <42277051+Charlie-McDaddy@users.noreply.github.com>
2023-12-30 09:26:23 +1000

    update styles

--- src/components/ModuleNav.jsx
+++ src/components/ModuleNav.jsx
@@ -5,10 +5,8 @@ const ModuleNav = () => {
   return (
     <div className="d-flex flex-row" id="module-nav">
       {MenuTitles.map((menu) => (
-        <div key={menu.id}>
-          <Link to={menu.link} className="nav-link" id="module-nav-link">
-            {menu.title}
-          </Link>
+        <div key={menu.id} className="text-wrap" id="module-nav-link">
+          <Link to={menu.link}>{menu.title}</Link>
         </div>
       ))}
     </div>
