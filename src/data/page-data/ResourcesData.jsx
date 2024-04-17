47c7a47bb84c2cee58b7c042865d30029bbf3d81
belindajp <142566486+belindajp@users.noreply.github.com>
2024-04-17 13:01:39 +1000

    resource page update

--- src/data/page-data/ResourcesData.jsx
+++ src/data/page-data/ResourcesData.jsx
@@ -18,6 +18,20 @@ const ResourcesData = () => {
         to speak to anyone yet, you can also use the Stop It Now! Australia live
         chat.{" "}
       </p>
+      <br></br>
+      <h2 className="secondary-color">Additional Support Services</h2>
+      <br></br>
+      <p>Crisis support - 1800RESPECT - 1800 727 732</p>
+      <p>Crisis support - Lifeline - 13 11 14</p>
+      <p>Crisis support (First Nations people) - 1800YARN - 13 92 76</p>
+      <p>Crisis support - Beyond Blue - 1300 22 4636</p>
+      <p>Counselling - Stop It Now! Australia - 180001 1800 </p>
+      <p>Counselling - MensLine - 1300 789 978</p>
+      <p>Counselling - QLife - LGBTQIA+ people - 1800 184 527</p>
+      <p>
+        Counselling - National counselling and referral service - Disabiltiy -
+        1800 421 468
+      </p>
       <br />
     </>
   );
