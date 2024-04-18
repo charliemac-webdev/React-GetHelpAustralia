960078e543147c0ba65229c9f964106a532fa22b
Charlie-McDaddy <42277051+Charlie-McDaddy@users.noreply.github.com>
2024-04-18 11:56:02 +1000

    make minicard folder font larger on lg screens

--- src/components/MiniCard.jsx
+++ src/components/MiniCard.jsx
@@ -2,7 +2,7 @@ const MiniCard = (props) => {
   return (
     <>
       <div className="folder">
-        <h3 className="secondary-color fs-7">{props.title}</h3>
+        <h3 className="secondary-color fs-lg-6">{props.title}</h3>
       </div>
     </>
   );
