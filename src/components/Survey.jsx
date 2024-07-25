20e8d6e8d7185f63454834945a3dc53d7420fd35
Charlie-McDaddy <42277051+Charlie-McDaddy@users.noreply.github.com>
2024-07-25 19:08:40 +1000

    update subject to name

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -39,7 +39,7 @@ const Survey = ({ nextRoute, formName }) => {
     e.preventDefault();
     const dataToSubmit = {
       "form-name": formName,
-      subject: `${formName} - ${timestamp}`,
+      name: `${formName} - ${timestamp}`,
       ...formData,
     };
     console.log("Submitting form data:", dataToSubmit);
@@ -69,7 +69,7 @@ const Survey = ({ nextRoute, formName }) => {
         <input type="hidden" name="form-name" value={formName} />
         <input
           type="hidden"
-          name="subject"
+          name="name"
           value={`${formName} - ${new Date().toISOString()}`}
         />
         <div className="row align-items-start pb-5">
