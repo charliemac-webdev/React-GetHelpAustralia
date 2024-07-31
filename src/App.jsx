0c31413a8cf69516aebb21f831f48746e46ae132
Charlie-McDaddy <42277051+Charlie-McDaddy@users.noreply.github.com>
2024-07-31 11:32:10 +1000

    disable analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./context/store";
 
 // Analytics
-import PostHogPageViewTracker from "./analytics/PostHogPageViewTracker";
-import GoogleAnalytics from "./analytics/GoogleAnalytics";
 
 // Components
+import AppRoutes from "./components/AppRoutes";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Help from "./components/Help";
 import ScrollToTop from "./components/ScrollToTop";
-import AppRoutes from "./components/AppRoutes";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="wrapper">
         <BrowserRouter>
-          <PostHogPageViewTracker />
-          <GoogleAnalytics />
+          {/* <PostHogPageViewTracker />
+          <GoogleAnalytics /> */}
           <ScrollToTop />
           <Header />
           <Help />
